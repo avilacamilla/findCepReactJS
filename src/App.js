b7e15f8435d260aafbdc0ee29709b2befb05c94a
Camilla Avila <avilacamilla@outlook.com>
2022-12-07 00:32:55 -0300

    Instalando a biblioteca de ícones - React Icons e adicionando animação com @keyframes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css'
+
 function App() {
   return (
     <div className="container">
-      <h1 className="title__cep">Buscador de CEP</h1>
+
+      <h1 className="title__cep">Localizar endereço por CEP</h1>
 
       <div className="container__input">
-        <input 
-        type="text"
-        placeholder="Digite o seu cep" 
-        />
+          <input 
+          type="text"
+          placeholder="Digite o seu cep" 
+          />
+        
+
+        <button className="button__search">
+          <FiSearch size={25} color="#FFFFFF" />
+      </button>
       </div>
 
-      <button className="button__search">
-        Procurar
-      </button>
+      <main className="main">
+        <h2>CEP: 23906485</h2>
+
+        <span>Rua Vasco da Gama</span>
+        <span>Complemento: Algum lugar</span>
+        <span>Vila Rosa</span>
+        <span>Rio de Janeiro - RJ</span>
+      </main>
+
+
     </div>
+
+    
+
+
   );
 }
 
