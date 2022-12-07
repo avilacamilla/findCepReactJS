54b3cf7f7e237c0f2c2c3b0b94eb286d484e6dcc
Camilla Avila <avilacamilla@outlook.com>
2022-12-07 00:01:28 -0300

    Estruturando esqueleto do HTML

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Testando projeto</h1>
+    <div className="container">
+      <h1 className="title__cep">Buscador de CEP</h1>
+
+      <div className="container__input">
+        <input 
+        type="text"
+        placeholder="Digite o seu cep" 
+        />
+      </div>
+
+      <button className="button__search">
+        Procurar
+      </button>
     </div>
   );
 }
