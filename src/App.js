1afa9ea1fe1eeaee2e1b3b1bfff30b4a5f17b1f4
Camilla Avila <avilacamilla@outlook.com>
2022-12-08 01:16:09 -0300

    Adicionando hook useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css'
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch() {
+    alert('Valor digitado: ' + input);
+  }
+
   return (
     <div className="container">
 
-      <h1 className="title__cep">Localizar endereço por CEP</h1>
+      <h1 className="title__cep">Busca CEP</h1>
 
       <div className="container__input">
           <input 
           type="text"
-          placeholder="Informe o seu CEP" 
+          placeholder="Informe o seu CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)} 
           />
         
 
-        <button className="button__search">
+        <button className="button__search" onClick={handleSearch}>
           <FiSearch size={25} color="#FFFFFF" />
       </button>
       </div>
